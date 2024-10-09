2c829a82a20ac9fc3528a4a2f06aa92f672bc870
jadavdhiraj020 <jadavdhiraj020@gmail.com>
2024-10-09 11:11:59 +0530

    last

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -32,7 +32,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-fallback-secret-key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
